fa212a16a38d2a2a0e718c0168ffc7f31d0e26c2
BatmanDark-knight <pascalayo66@gmail.com>
2025-02-13 17:24:36 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Bot',
   botName: "Dark-Knight",
   botOwner: 'Adekunle The Stallion', // Your name
-  ownerNumber: '+2349153181201', //Your number
+  ownerNumber: '9153181201', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
